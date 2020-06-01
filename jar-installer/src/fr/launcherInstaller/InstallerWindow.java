2c1ff4f42172c6d4828d29c7a6636a3b31efcc6d
baztoul76 <66006344+baztoul76@users.noreply.github.com>
2020-06-01 17:54:59 +0200

    Create InstallerWindow.java

---
+++ jar-installer/src/fr/launcherInstaller/InstallerWindow.java
@@ -0,0 +1,20 @@
+package fr.launcherInstaller;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import javax.awt.Color;
+
+public class InstallerWindow extends JFrame{
+  JPanel pan = new JPanel();
+  public InstallerWindow(String title, int size1, int size2) {
+    this.setTitle(title);
+    this.setResizable(false);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(size1, size2);
+    this.setLocationRelativeTo(null);
+    pan.setBackground(Color.CYAN);
+    
+    this.setVisible(true);
+  }
+}
