40925f86b2d083993828e0268e19b943775ed990
baztoul76 <66006344+baztoul76@users.noreply.github.com>
2020-06-19 12:34:27 +0200

    Update Installer.java

--- jar-installer/src/fr/launcherInstaller/Installer.java
+++ jar-installer/src/fr/launcherInstaller/Installer.java
@@ -2,6 +2,6 @@ package fr.installer;
 
 public class Installer {
 	public static void main(String[] args) {
-		InstallButton install = new InstallButton("launch");
+		InstallFrame installer = new InstallFrame("launch");
 	}
 }
