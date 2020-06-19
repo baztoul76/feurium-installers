027f4821adec44d65bc33dca85b62225c874f644
baztoul76 <66006344+baztoul76@users.noreply.github.com>
2020-06-19 12:35:25 +0200

    Update Installer.java

--- jar-installer/src/fr/launcherInstaller/Installer.java
+++ jar-installer/src/fr/launcherInstaller/Installer.java
@@ -2,6 +2,6 @@ package fr.installer;
 
 public class Installer {
 	public static void main(String[] args) {
-		InstallFrame installer = new InstallFrame("launch");
+		Installerrame installer = new InstallerFrame("launch");
 	}
 }
